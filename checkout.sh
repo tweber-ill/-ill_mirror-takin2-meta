e6a4e6f33cff6f28f4acbfa5ffa309a1d7bce5bc
Tobias WEBER <tweber@ill.fr>
2020-07-29 08:37:15 +0200

    added a small checkout script

---
+++ checkout.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# checkout all of takin's repos
+# @author Tobias Weber <tweber@ill.fr>
+# @date jul-20
+# @license GPLv2
+#
+
+if [ -L takin ] || [ -d takin ]; then
+	echo -e "A takin directory already exists here, exiting."
+	exit -1
+fi
+
+
+mkdir takin
+cd takin
+
+git clone https://code.ill.fr/scientific-software/takin/meta.git
+git clone https://code.ill.fr/scientific-software/takin/core.git
+git clone https://code.ill.fr/scientific-software/takin/mag-core.git
+git clone https://code.ill.fr/scientific-software/takin/tlibs.git
+git clone https://code.ill.fr/scientific-software/takin/tlibs2.git
+git clone https://code.ill.fr/scientific-software/takin/data.git
+#git clone https://code.ill.fr/tweber/takin-mnsi.git
