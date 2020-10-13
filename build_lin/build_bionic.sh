ef4a34bacc9d5ed462392923903b9ba4ccbcca51
Tobias WEBER <tweber@ill.fr>
2020-10-13 10:26:05 +0200

    updated build script

--- build_lin/build_bionic.sh
+++ build_lin/build_bionic.sh
@@ -9,10 +9,10 @@
 # individual building steps
 setup_buildenv=1
 setup_externals=1
-setup_externals2=0
+setup_externals2=1
 build_externals=1
 build_takin=1
-build_takin2=0
+build_takin2=1
 build_package=1
 
 
