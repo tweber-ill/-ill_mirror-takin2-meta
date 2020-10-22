a6ec6f8f3495c8d3c4535699f25ac9dca44a410c
Tobias WEBER <tweber@ill.fr>
2020-10-22 11:35:57 +0200

    updated checkout script

--- checkout.sh
+++ checkout.sh
@@ -6,31 +6,56 @@
 # @license GPLv2
 #
 
-if [ -L takin ] || [ -d takin ]; then
-	echo -e "A takin directory already exists here, exiting."
+echo -e "================================================================================"
+echo -e "Setting up Takin 2 sources"
+echo -e "================================================================================"
+
+
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "Preliminary steps..."
+echo -e "--------------------------------------------------------------------------------"
+
+GIT="$(which git)"
+if [ "$GIT" = "" ]; then
+	echo -e "\nERROR: The \"git\" tool was not found, please install it before running this script.\n"
 	exit -1
 fi
 
 
-mkdir takin
-cd takin
+TAKIN_DIR=takin
+if [ -L ${TAKIN_DIR} ] || [ -d ${TAKIN_DIR} ]; then
+	echo -e "\nERROR: A \"${TAKIN_DIR}\" directory already exists here, exiting.\n"
+	exit -1
+fi
+
+mkdir -v ${TAKIN_DIR}
+cd ${TAKIN_DIR}
 
 
-# clone all sub-repos
-git clone https://code.ill.fr/scientific-software/takin/meta.git
-git clone https://code.ill.fr/scientific-software/takin/core.git
-git clone https://code.ill.fr/scientific-software/takin/mag-core.git
-git clone https://code.ill.fr/scientific-software/takin/tlibs.git
-git clone https://code.ill.fr/scientific-software/takin/tlibs2.git
-git clone https://code.ill.fr/scientific-software/takin/data.git
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "Cloning all Takin repositories..."
+echo -e "--------------------------------------------------------------------------------"
+
+${GIT} clone https://code.ill.fr/scientific-software/takin/meta.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/core.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/mag-core.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/tlibs.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/tlibs2.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/data.git
 
 mkdir plugins
 pushd plugins
-git clone https://code.ill.fr/scientific-software/takin/plugins/mnsi.git
+${GIT} clone https://code.ill.fr/scientific-software/takin/plugins/mnsi.git
 popd
 
 
-# make links
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "Creating links..."
+echo -e "--------------------------------------------------------------------------------"
+
 pushd core
 ln -sf ../data
 ln -sf ../tlibs
@@ -39,3 +64,9 @@ popd
 pushd mag-core
 ln -sf ../tlibs2
 popd
+
+
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "Done, the Takin source can be found in \"${TAKIN_DIR}\"."
+echo -e "--------------------------------------------------------------------------------\n"
