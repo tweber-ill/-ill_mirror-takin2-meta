3ace07f3854d78696458f7c09872a7f23b68c0ef
Tobias WEBER <tweber@ill.fr>
2020-11-03 10:55:52 +0100

    undo accidental commit

--- build_mac/build_brew.sh
+++ build_mac/build_brew.sh
@@ -8,11 +8,11 @@
 
 
 # individual building steps
-setup_buildenv=0
-setup_externals=0
-setup_externals2=0
-build_takin=0
-build_takin2=0
+setup_buildenv=1
+setup_externals=1
+setup_externals2=1
+build_takin=1
+build_takin2=1
 build_package=1
 
 
