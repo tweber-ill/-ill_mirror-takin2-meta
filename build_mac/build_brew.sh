bded0033992d1b6121bd77208a271fa743d71000
Tobias WEBER <tweber@ill.fr>
2022-01-23 20:08:31 +0100

    added magdyn tool

--- build_mac/build_brew.sh
+++ build_mac/build_brew.sh
@@ -121,6 +121,7 @@ if [ $build_takin2 -ne 0 ]; then
 		cp -v tools/pol/takin_pol "${TAKIN_ROOT}"/core/bin/
 		cp -v tools/structfact/takin_structfact "${TAKIN_ROOT}"/core/bin/
 		cp -v tools/magstructfact/takin_magstructfact "${TAKIN_ROOT}"/core/bin/
+		cp -v tools/magdyn/takin_magdyn "${TAKIN_ROOT}"/core/bin/
 		cp -v tools/scanbrowser/takin_scanbrowser "${TAKIN_ROOT}"/core/bin/
 		cp -v tools/magsgbrowser/takin_magsgbrowser "${TAKIN_ROOT}"/core/bin/
 		cp -v tools/moldyn/takin_moldyn "${TAKIN_ROOT}"/core/bin/
